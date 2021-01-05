3f9712f6387622336a77a4f90acbe6ce7102c91d
gakigakigakigakigaki <73731407+gakigakigakigakigaki@users.noreply.github.com>
2021-01-05 14:47:08 +0900

    変更4

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,25 +1,25 @@
 class RelationshipsController < ApplicationController
-  before_action :set_user
+  # before_action :set_user
 
-  def create
-    following = current_user.follow(@user)
-    if folowing.save
-      flash[:success]  = '企業をフォローしました'
-      redirect_to @user
-    else
-      flash.now[:alert] = '企業のフォローに失敗しました'
-      redirect_to @user
-    end
+  # def create
+  #   following = current_user.follow(@user)
+  #   if folowing.save
+  #     flash[:success]  = '企業をフォローしました'
+  #     redirect_to @user
+  #   else
+  #     flash.now[:alert] = '企業のフォローに失敗しました'
+  #     redirect_to @user
+  #   end
 
-  def destroy
-    following = current_user.unfollow(@user)
-    if following.destroy
-      flash[:success] = '企業のフォローを解除しました'
-      redirect_to @user
-    else
-      flash.now[:alert] = '企業のフォロー解除に失敗しました'
-      redirect_to @user
-    end
+  # def destroy
+  #   following = current_user.unfollow(@user)
+  #   if following.destroy
+  #     flash[:success] = '企業のフォローを解除しました'
+  #     redirect_to @user
+  #   else
+  #     flash.now[:alert] = '企業のフォロー解除に失敗しました'
+  #     redirect_to @user
+  #   end
 end
 
 # private
